2e1fa385b8f5e2719bc8e7175a47068b3bd0bd2c
Leonardo Ribeiro <leon_fritas@hotmail.com>
2022-08-31 09:37:25 -0400

    Corrigindo identação do codigo

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -3,11 +3,6 @@ import { baseUrl } from "/src/scripts/variables.js";
 async function getUser(userName) {
     const response = await fetch(`${baseUrl}/${userName}`);
     return await response.json();
-    
-    
-    
   }
   
-
-
   export {getUser}
